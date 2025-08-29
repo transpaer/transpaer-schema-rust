a2969cb55fca063eebd03953a2dc6e6cabaf4308
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2025-08-29 17:08:57 +0200

    Add license file and license notices

--- transpaer-schema/src/merge.rs
+++ transpaer-schema/src/merge.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use std::collections::HashSet;
 
 // TODO: should error if strings are not the same.
